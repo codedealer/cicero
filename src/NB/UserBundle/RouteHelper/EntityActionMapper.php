210db7714a20eed37cd3acc6972645d4e76fe4b5
codedealer <work.mello@gmail.com>
2015-03-30 02:38:51 +0300

    add entityActionMapper service

---
+++ src/NB/UserBundle/RouteHelper/EntityActionMapper.php
@@ -0,0 +1,31 @@
+<?php
+namespace NB\UserBundle\RouteHelper;
+
+class EntityActionMapper
+{
+	private $actionMap = [];
+
+	public function registerAction($entityName, $actionType, $action){
+		if(is_callable($action)){
+			if(is_array($this->actionMap[$entityName])){
+				$this->actionMap[$entityName][$actionType] = $action;
+			}
+			else{
+				$this->actionMap[$entityName] = [$actionType => $action];
+			}
+			return $this;
+		}
+		else
+			throw new \Exception('Third argument is expected to be callable');
+	}
+
+	public function map($entityName, $actionType){
+		if(array_key_exists($entityName, $this->actionMap) 
+			&& is_array($this->actionMap[$entityName])
+			&& array_key_exists($actionType, $this->actionMap)){
+			return $this->actionMap[$entityName][$actionType];
+		}
+		return false;
+	}
+}
+?>
