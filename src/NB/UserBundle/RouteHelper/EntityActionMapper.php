2830343314a9ff6df59c9178bcc6c506be513e74
codedealer <work.mello@gmail.com>
2015-04-16 19:21:12 +0300

    add name normalization

--- src/NB/UserBundle/RouteHelper/EntityActionMapper.php
+++ src/NB/UserBundle/RouteHelper/EntityActionMapper.php
@@ -6,7 +6,7 @@ class EntityActionMapper
 	private $actionMap = [];
 
 	public function registerAction($entityName, $actionType, $action){
-		
+			$entityName = $this->normilize($entityName);
 			if(array_key_exists($entityName, $this->actionMap) 
 				&& is_array($this->actionMap[$entityName])){
 				$this->actionMap[$entityName][$actionType] = $action;
@@ -18,12 +18,17 @@ class EntityActionMapper
 	}
 
 	public function map($entityName, $actionType){
+		$entityName = $this->normilize($entityName);
 		if(array_key_exists($entityName, $this->actionMap) 
 			&& is_array($this->actionMap[$entityName])
 			&& array_key_exists($actionType, $this->actionMap[$entityName])){
 			return $this->actionMap[$entityName][$actionType];
 		}
 		return false;
+	}
+
+	private function normilize($string){
+		return str_replace(['\\','_'], '', strtolower($string));
 	}
 }
 ?>
